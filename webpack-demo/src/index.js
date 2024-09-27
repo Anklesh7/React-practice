c3245cfbdcd28ef961f5c69bdf82f1df4617f316
Anklesh7 <163126701+Anklesh7@users.noreply.github.com>
2024-09-27 16:42:52 +0530

    Delete webpack-demo directory

--- webpack-demo/src/index.js
+++
@@ -1,6 +0,0 @@
-import './style.css';
-
-console.log('Hello, Webpack is working!');
-
-// Example of changing content dynamically
-document.querySelector('h1').innerText = "This is my Webpack project!";
